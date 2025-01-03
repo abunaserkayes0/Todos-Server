a8057c4e28010fae272e4d76def475259c88072a
abunaserkayes0 <20234203080@cse.bubt.edu.bd>
2025-01-03 11:45:46 +0600

    update env file

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.xubpy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `${process.env.MONGO_URI}`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
